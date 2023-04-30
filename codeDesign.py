e645e4da8c00ef3e3eafe136b8735aaed5421e90
MarcoStucchi <marco.stucchi@gmail.com>
2023-04-30 18:44:39 +0200

    changed code name to avoid conflicts
    (cherry picked from commit 5d861b0c3224b79d1c47483f2577a391f1dae73f)

---
+++ codeDesign.py
@@ -0,0 +1,42 @@
+import datasetAlarm as dsa
+import pandas as pd
+import csv
+
+dtypes = {
+    "timestamp": "category",
+    "alarm": "category",
+    "serial": "category",
+}
+
+if __name__ == "__main__":
+    
+    my_params = {}
+    my_params['windows_input']=[480,960,240]
+    my_params['windows_output']=[240]
+    my_params['min_counts']=[3]
+    my_params['sigmas']=[3]
+    my_params['offsets']=[230]
+    my_params['verbose']=True
+    #filename = "alarms.csv"
+    #names = ["timestamp", "alarm", "serial"]
+    #data = pd.read_csv(filename, names=names)
+    #print(data.shape)
+    params = []
+    '''
+    df = pd.read_csv(
+        "groupby-data/legislators-historical.csv",
+        dtype=dtypes,
+        usecols=list(dtypes) + ["birthday", "last_name"],
+        parse_dates=["birthday"]
+    )
+    '''
+    df = pd.read_csv(
+        r'alarms.csv',
+        dtype= dtypes,
+        delimiter= ','
+    )
+    print(df)
+    #create_datasets(params_list, start_point=0)
+    #several combinations of parameters are created, each combination can be considered as a new dataset
+    #params_list = dsa.create_params_list(data_path="", params=my_params, verbose=True)
+    dsa.create_datasets(df, my_params, start_point=0, file_tag='all_alarms')
